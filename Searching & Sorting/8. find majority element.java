a5fc68f6e6038e577d6ff7efd56b46fa34fc72e1
Meet Patel <81353451+mkpatel-247@users.noreply.github.com>
2023-02-03 15:41:22 +0530

    Boore Voting Algorithm

---
+++ Searching & Sorting/8. find majority element.java
@@ -0,0 +1,29 @@
+//Boore Voting Algorithm
+class Solution
+{
+    static int majorityElement(int a[], int size)
+    {
+        // your code here
+        int cnt = 0;
+        int el = 0;
+        for(int n : a){
+            if(cnt == 0)
+                el = n;
+            if(n == el)
+                cnt++;
+            else
+                cnt--;
+        }
+        
+        cnt = 0;
+        for(int n : a){
+            if(el == n){
+                cnt++;
+            }
+        }
+        if(cnt > size/2) 
+          return el;
+        else 
+          return -1;
+    }
+}
