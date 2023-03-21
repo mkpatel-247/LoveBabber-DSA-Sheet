e1d75bade2443befea7931edeb408d98c5ff3bf7
Meet Patel <81353451+mkpatel-247@users.noreply.github.com>
2023-03-22 01:39:10 +0530

    Update 04. Write a program to Delete loop in a linked list..java

--- Linked List/04. Write a program to Delete loop in a linked list..java
+++ Linked List/04. Write a program to Delete loop in a linked list..java
@@ -1,1 +1,33 @@
+/*
+class Node
+{
+    int data;
+    Node next;
+}
+*/
 
+class Solution
+{
+    //Function to remove a loop in the linked list.
+    public static void removeLoop(Node head){
+        // code here
+        // remove the loop without losing any nodes
+        if(head==null){
+            return;
+        }
+        // code here
+        HashSet<Node> arr = new HashSet<>();
+        Node start = head;
+        arr.add(start);
+        
+        while(start != null){
+            if(!arr.contains(start.next)){
+                start = start.next;
+                arr.add(start);
+            } else {
+                start.next = null;
+                start = start.next;
+            }
+        }
+    }
+}
