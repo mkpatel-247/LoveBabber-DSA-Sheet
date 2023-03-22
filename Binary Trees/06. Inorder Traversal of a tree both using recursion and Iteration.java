4554ae37ef4e83aa572d910aeb12b86546936be1
Meet Patel <81353451+mkpatel-247@users.noreply.github.com>
2023-03-22 23:06:06 +0530

    Update 06. Inorder Traversal of a tree both using recursion and Iteration.java

--- Binary Trees/06. Inorder Traversal of a tree both using recursion and Iteration.java
+++ Binary Trees/06. Inorder Traversal of a tree both using recursion and Iteration.java
@@ -1,1 +1,56 @@
 //Platform LeetCode
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+/*--------------------Using Recursion Approach-------------------*/
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        inorder(root, ans);
+        return ans;
+    }
+
+    private void inorder(TreeNode root, List<Integer> ans){
+        if(root == null){
+            return;
+        }
+        
+        inorder(root.left, ans);
+        ans.add(root.val);
+        inorder(root.right, ans);
+    }
+}
+
+/*--------------------Using Iterative Approach-------------------*/
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+
+        Stack<TreeNode> st = new Stack<TreeNode>();
+        TreeNode cur = root;
+        while(cur != null || !st.empty()){
+            while(cur != null){
+                st.add(cur);
+                cur = cur.left;
+            }
+            cur = st.pop();
+            ans.add(cur.val);
+            cur = cur.right;
+        }
+
+        return ans;
+    }
+}
