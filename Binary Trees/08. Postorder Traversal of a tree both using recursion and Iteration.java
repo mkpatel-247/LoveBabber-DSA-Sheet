186083ba398f4bfa71625f3887a1c9a29590aa24
Meet Patel <81353451+mkpatel-247@users.noreply.github.com>
2023-04-04 23:23:04 +0530

    Update 08. Postorder Traversal of a tree both using recursion and Iteration.java

--- Binary Trees/08. Postorder Traversal of a tree both using recursion and Iteration.java
+++ Binary Trees/08. Postorder Traversal of a tree both using recursion and Iteration.java
@@ -13,6 +13,38 @@
  *     }
  * }
  */
+/*---------- Iterative Approach ------------*/
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null) return res;
+
+        Stack<TreeNode> s = new Stack<>();
+        s.push(root);
+        TreeNode curr;
+
+        while (!s.isEmpty()) {
+
+            curr = s.peek();
+            if (curr.left != null) {
+                s.push(curr.left);
+                curr.left = null;
+            }
+            else if (curr.right != null) {
+                s.push(curr.right);
+                curr.right = null;
+            }
+            else {
+                curr = s.pop();
+                res.add(curr.val);
+            }
+        }
+
+        return res;
+    }
+}
+
+/*---------- Recursive Approach ------------*/
 class Solution {
     public List<Integer> postorderTraversal(TreeNode root) {
         ArrayList<Integer>ans=new ArrayList<>();
