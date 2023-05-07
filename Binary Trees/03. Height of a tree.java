8e4329190ac50894bf05c0bc2534d7dbfd403996
Meet Patel <81353451+mkpatel-247@users.noreply.github.com>
2023-05-07 22:51:51 +0530

    Create 03. Height of a tree.java

---
+++ Binary Trees/03. Height of a tree.java
@@ -0,0 +1,27 @@
+/* 
+class Node
+{
+    int data;
+    Node left, right;
+
+    Node(int item)
+    {
+        data = item;
+        left = right = null;
+    }
+}
+ */
+
+class Solution {
+    //Function to find the height of a binary tree.
+    int height(Node node) 
+    {
+        // code here 
+        if(node==null){
+            return 0;
+        }
+        int l=height(node.left);
+        int r=height(node.right);
+        return Math.max(l,r)+1;
+    }
+}
