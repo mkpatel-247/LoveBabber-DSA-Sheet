8d199944563e941104c6e2c1a0a40450adee3586
Meet Patel <81353451+mkpatel-247@users.noreply.github.com>
2023-07-07 22:03:56 +0530

    Update 12. Merge 2 sorted arrays without using Extra space..java

--- Array/12. Merge 2 sorted arrays without using Extra space..java
+++ Array/12. Merge 2 sorted arrays without using Extra space..java
@@ -14,3 +14,30 @@ class Solution {
         }
     }
 }
+
+// GFG Problem Link: https://practice.geeksforgeeks.org/problems/merge-two-sorted-arrays-1587115620/1
+class Solution
+{
+    //Function to merge the arrays.
+    public static void merge(long arr1[], long arr2[], int n, int m) 
+    {
+        // code here 
+        int l = n-1;
+        int r = 0;
+        
+        while(l >= 0 && r < m){
+            if(arr1[l] > arr2[r]){
+                long swap = arr1[l];
+                arr1[l] = arr2[r];
+                arr2[r] = swap;
+                l--;
+                r++;
+            }else{
+                break;
+            }
+        }
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+    }
+}
+
